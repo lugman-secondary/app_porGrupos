183ce671fbfdc83c29eb3211bf33e3f3f98a0c06
alumno <lugmansito@gmail.com>
2018-01-19 11:38:49 +0100

    Main layout añadido

--- app/src/main/java/es/com/lugman/aplicaciongrupod/MainActivity.java
+++ app/src/main/java/es/com/lugman/aplicaciongrupod/MainActivity.java
@@ -1,16 +1,34 @@
 package es.com.lugman.aplicaciongrupod;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnViewProducts;
+    Button btnNewProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ERROR
+        btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
+        btnNewProduct = (Button) findViewById(R.id.btnCreateProduct);
+
+
+        btnViewProducts.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Launching All products Activity
+//                Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+//                startActivity(i);
+
+            }
+        });
 
     }
 }
